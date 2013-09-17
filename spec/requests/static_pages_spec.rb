21ac835496e46acdb0b60b4bf7613b9732869aaf
Quoc Viet <viet161184@gmail.com>
2013-09-17 10:12:30 -0400

    chapter3,test,slightly dynamic title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,5 +6,36 @@ describe "StaticPages" do
      visit '/static_pages/home'
      expect(page).to have_content('Sample App')
      end
+     
+    it "should have the title 'home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Home')
+    end 
   end
+  
+  describe "HelpPages" do
+    it "Should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    
+    it "should have the title 'help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on rails Tutorial Sample App | Help')
+    end  
+  end
+  
+  describe "AboutPages" do
+    it "Should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About Us')
+    end  
+  end
+  
 end
+
